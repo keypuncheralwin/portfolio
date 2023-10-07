805d90413752d02db2e604aadad878a59c736f0d
Alwin George <alwingeorge11@gmail.com>
2023-10-08 01:38:14 +1030

    Update next config file

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {}
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-// module.exports = nextConfig
-module.exports = {
-    // Other Next.js config options...
-  
-    experimental: {
-      output: 'export',
-    },
-  };
-  
+module.exports = nextConfig
