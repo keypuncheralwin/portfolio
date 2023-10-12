74a0ede4d7731ba56542678704a42c79fbeeff9b
Alwin George <alwingeorge11@gmail.com>
2023-10-12 21:06:10 +1030

    Add Experience section

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth">
       <body
-        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36`}
+        className={`${inter.className} bg-gray-300 text-gray-950 relative pt-28 sm:pt-36 h-[5000px]`}
       >
         <div className="bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div
--- lib/data.ts
+++ lib/data.ts
@@ -1,4 +1,8 @@
 import lavaRising from '@/public/lavaRising.png';
+import React from 'react';
+import { CgWorkAlt } from 'react-icons/cg';
+import { FaReact } from 'react-icons/fa';
+import { LuGraduationCap } from 'react-icons/lu';
 
 export const links = [
   {
@@ -70,4 +74,31 @@ export const skillsData = [
   'Linux/Bash',
   'Unity (2d/3d game dev)',
   'Unit/End-to-end testing (Jest/Cypress)',
+] as const;
+
+export const experiencesData = [
+  {
+    title: 'Lorem Ipsum',
+    location: 'Dolor Sit, AM',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.',
+    icon: React.createElement(LuGraduationCap),
+    date: 'Lorem Date',
+  },
+  {
+    title: 'Consectetur Adipiscing',
+    location: 'Elit, LM',
+    description:
+      'Donec ac odio tempor orci dapibus ultrices in iaculis nunc sed augue lacus. Nisl nisi scelerisque eu ultrices vitae auctor eu augue ut.',
+    icon: React.createElement(CgWorkAlt),
+    date: 'Ipsum - Dolor',
+  },
+  {
+    title: 'Sed Do Eiusmod',
+    location: 'Tempor, TX',
+    description:
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.',
+    icon: React.createElement(FaReact),
+    date: 'Lorem - Ipsum',
+  },
 ] as const;
