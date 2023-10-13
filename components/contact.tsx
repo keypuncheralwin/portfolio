ecb82ac590ee405f0e1a6fa2769a0d8cd07a7134
Alwin George <alwingeorge11@gmail.com>
2023-10-13 18:08:41 +1030

    Add footer section

--- components/contact.tsx
+++ components/contact.tsx
@@ -33,8 +33,8 @@ export default function Contact() {
 
       <p className="text-gray-700 -mt-6">
         Please contact me directly at{' '}
-        <a className="underline" href="mailto:example@gmail.com">
-          example@gmail.com
+        <a className="underline" href="mailto:alwingeorge8@gmail.com">
+          alwingeorge8@gmail.com
         </a>{' '}
         or through this form.
       </p>
