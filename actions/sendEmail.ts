10668f5e862bf59303dc2e4461f160ca42f80fba
keypuncheralwin <alwingeorge11@gmail.com>
2024-02-29 10:26:22 +1030

    Update email contact address

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -27,7 +27,7 @@ export const sendEmail = async (formData: FormData) => {
   try {
     data = await resend.emails.send({
       from: process.env.EMAIL_FROM as string,
-      to: 'alwingeorge11@gmail.com',
+      to: 'alwingeorge8@gmail.com',
       subject: 'Message from contact form',
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
